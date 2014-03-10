ed7a9adadb92d0b149140fcc5de5b6f024cef2c5
Nathan Cook <nathancookster@gmail.com>
2014-03-10 13:57:20 -0500

    Better undefined check for range, add test.

--- src/validator.js
+++ src/validator.js
@@ -782,7 +782,7 @@
     Range: function ( min, max ) {
       this.__class__ = 'Range';
 
-      if ( min === undefined || max === undefined )
+      if ( typeof(min) === undefined || typeof(max) === undefined )
         throw new Error( 'Range assert expects min and max values' );
 
       this.min = min;
