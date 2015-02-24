39d0a07d1e6e49e1af3c00fb1564128c4aa83db8
Mark McEver <mark@fastmail.net>
2015-02-24 15:34:27 -0600

    Fixed a very minor typo.

--- src/validator.js
+++ src/validator.js
@@ -632,7 +632,7 @@
       this.__class__ = 'Length';
 
       if ( !boundaries.min && !boundaries.max )
-        throw new Error( 'Lenth assert must be instanciated with a { min: x, max: y } object' );
+        throw new Error( 'Length assert must be instanciated with a { min: x, max: y } object' );
 
       this.min = boundaries.min;
       this.max = boundaries.max;
